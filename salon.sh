4f568e07a5769abeea676f4af922ee94c0910bfc
buitragg <gmbuitrago10@gmail.com>
2024-07-12 10:19:19 -0700

    cleaned up some ID and added more comments

--- salon.sh
+++ salon.sh
@@ -4,7 +4,7 @@ PSQL="psql --username=freecodecamp --dbname=salon --tuples-only -c"
 #PRINT TITLE AND NAME OF SALON
 
 TRUNCATE=$($PSQL "TRUNCATE customers, appointments")
-echo -e "\n Welcome to Mojito Salon. How can we help you today?\n"
+echo -e "\n Welcome to Mojito Salon. How can we help you today?"
 
 
 # menu function will display all of the services
@@ -22,18 +22,13 @@ MAIN_MENU (){
   #display services and id's
   echo "$SERVICES" | while read ID BAR NAME
   do 
-    if [[ $ID = 4 ]]
-    then
-      echo -e "$ID) $NAME\n"
-    else
-      echo "$ID) $NAME"
-    fi
+      echo -e "\n$ID) $NAME"
 
   done
 
   #read 
-  read SERVICE
-  SERVICE_SELECTED=$($PSQL "SELECT name FROM services WHERE service_id = '$SERVICE'")
+  read SERVICE_ID_SELECTED
+  SERVICE_SELECTED=$($PSQL "SELECT name FROM services WHERE service_id = '$SERVICE_ID_SELCTED'")
   case $SERVICE in
     1) SERVICES $SERVICE_SELECTED;;
     2) SERVICES $SERVICE_SELECTED;;
@@ -47,16 +42,20 @@ MAIN_MENU (){
 SERVICES() {
   SERVICE_SELECTED=$1
   SERVICE_ID_SELECTED=$($PSQL "SELECT service_id FROM services WHERE name = '$SERVICE_SELECTED'")
+
   echo -e "\nPlease answer all of the questions below to schedule your appointment:\n"
 
   #ask for phone number 
   echo -e "What's your phone number? (Ex. 555-555-5555)"
   read PHONE_NUMBER
+
   # check if number is in the correct format 
   case "$PHONE_NUMBER" in
   [1-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]) ;;
-  *) SERVICES "Please add a correct phone number";;
+  *) SERVICES "Please add a correct phone number:";;
   esac
+
+  # get customer name using phone number 
   CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$PHONE_NUMBER'")
   
   #if number not in database
@@ -65,11 +64,27 @@ SERVICES() {
     # get customer name 
     echo -e "\nThere is not a record with that phone number, what's your name?"
     read NAME
-    # add to customers table 
+    # add new to customers table 
     INSERT_CUSTOMER=$($PSQL "INSERT INTO customers(name, phone) VALUES ('$NAME', '$PHONE_NUMBER')")
   fi
+  CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$PHONE_NUMBER'")
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$PHONE_NUMBER'")
+  
 
   #ask about time for appointment 
+  echo -e "\nWhat time would you like you $SERVICE_SELECTED,$CUSTOMER_NAME?"
+  read TIME
+
+
+  #add apointment 
+  INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES ($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$TIME')")
+  echo -e "\nI have put you down for a $SERVICE_SELECT at $TIME, $CUSTOMER_NAME\n"
+
+  MAIN_MENU "Welcome to Mojito Salon. How can we help you today?"
+
+
+
+
   
 }
 
